68f020d87e70ee0f15c5400dd7651e632a445469
“kaufast” <kaufast@gmail.com>
2024-12-11 17:39:20 +0100

    del Google Tag Manager and Analytics scripts

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,21 @@ import "../public/assets/css/vendor.css";
 import "../public/assets/sass/style.scss";
 import { ParallaxProvider } from "react-scroll-parallax";
 import ScrollTop from "@/components/common/ScrollTop";
-import { Poppins } from "next/font/google";
+import { Unbounded, Poppins } from "next/font/google";
 import ScrollTopBehaviour from "@/components/common/ScrollTopBehavier";
 
-// Google Fonts
+if (typeof window !== "undefined") {
+  import("bootstrap/dist/js/bootstrap.esm").then((module) => {
+    // Bootstrap functionality can be accessed here
+  });
+}
+
+const unbounded = Unbounded({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--title-font",
+});
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
@@ -31,18 +42,11 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="es">
-      <head>
-        {/* Cookie-Script */}
-        <script
-          type="text/javascript"
-          charSet="UTF-8"
-          src="https://cdn.cookie-script.com/s/041f0acc3eb9ea6bcb191f2b4322dcf9.js"
-        ></script>
-      </head>
-      <body className={`body ${poppins.variable}`}>
+      <body className={`body ${poppins.variable} ${unbounded.variable}`}>
         <ParallaxProvider>{children}</ParallaxProvider>
         <ScrollTop />
         <ScrollTopBehaviour />
+
       </body>
     </html>
   );
