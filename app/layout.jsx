6cdd741296344dd5250957136f461e6f6557fe9c
“kaufast” <kaufast@gmail.com>
2024-12-17 13:58:55 +0100

    added chat

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,24 +8,22 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import ScrollTop from "@/components/common/ScrollTop";
 import { Unbounded, Poppins } from "next/font/google";
 import ScrollTopBehaviour from "@/components/common/ScrollTopBehavier";
-import Head from 'next/head';
-import { GoogleTagManager } from '@next/third-parties/google' 
-
+import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 if (typeof window !== "undefined") {
   import("bootstrap/dist/js/bootstrap.esm").then((module) => {
-    // Module is imported, you can access any exported functionality if
+    // Module is imported
   });
 }
-// wow js
 
+// wow js
 const unbounded = Unbounded({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--title-font",
 });
 
-// Poppins font
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
@@ -35,6 +33,7 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   const path = usePathname();
   let wow = null;
+
   useEffect(() => {
     const { WOW } = require("wowjs");
     wow = new WOW({
@@ -43,29 +42,48 @@ export default function RootLayout({ children }) {
     });
     wow.init();
   }, [path]);
-  //useEffect(() => {
 
-  //wow?.sync();
-  //}, [path]);
   return (
     <html lang="es">
-            <GoogleTagManager gtmId="GTM-NJHK9DQ4" />
-      <body className={`body  ${poppins.variable} ${unbounded.variable}`}>
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJHK9DQ4"
-        height="0" 
-        width="0" 
-        style={{ display: "none", visibility: "hidden" }}>
-        </iframe>
-      </noscript>
-        <ParallaxProvider>
+      <GoogleTagManager gtmId="GTM-NJHK9DQ4" />
+      <body className={`body ${poppins.variable} ${unbounded.variable}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJHK9DQ4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
 
-          {children}
+        <ParallaxProvider>{children}</ParallaxProvider>
 
-          </ParallaxProvider>
         <ScrollTop />
         <ScrollTopBehaviour />
-        <script type="text/javascript" charSet="UTF-8" src="//cdn.cookie-script.com/s/041f0acc3eb9ea6bcb191f2b4322dcf9.js"></script>
+
+        {/* Tawk.to Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function() {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/6761469649e2fd8dfef95b62/1if9vnet4';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
+
+        {/* Cookie Script */}
+        <script
+          type="text/javascript"
+          charSet="UTF-8"
+          src="//cdn.cookie-script.com/s/041f0acc3eb9ea6bcb191f2b4322dcf9.js"
+        ></script>
       </body>
     </html>
   );
